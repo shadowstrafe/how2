0db46d8ce1923332b4a70869928eb78552dca424
Lee Chengkai <chengkai@tecedgesolutions.com>
2017-11-08 12:49:08 +0800

    Add command line interface

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Command Line Interface goes here
+var db = require('./build/how2db.js');
+
+// var argparse = require('argparse').ArgumentParser;
+var program = require('commander');
+
+program.version('0.0.1')
+    .option('--category, -c', 'Limit the search to the provided CATEGORY')
+    .parse(process.argv);
+
+var tags = program.args;
+var category = program.category;
+
+if (tags.length > 0) {
+    if (tags.length == 1) {
+        console.log(`Searching for entries that have the tag '${tags[0]}'.`);
+    } else {
+        console.log(`Searching for entries that have all the tags in (${tags.join(',')}).`);
+    }
+}
+
+if (category) {
+    console.log(`Searching within the category '${category}'.`);
+}
+
+db.Open();
+db.Get(category, tags, function (err, rows) {
+    console.log(rows);
+});
+db.Close();
