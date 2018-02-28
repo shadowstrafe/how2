98b6b58198150b6426e847a0c971276132498af1
Lee Chengkai <chengkai@tecedgesolutions.com>
2018-02-28 14:21:48 +0800

    Remove dependency on gulp for building

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
 var inquirer = require('inquirer');
 var open = require('opn');
+var del = require('rimraf');
 var path = require('path');
+var slash = require('slash');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var frontMatter = require('front-matter');
+var glob = require('glob');
 var program = require('commander');
 var url = require('url');
 require('pkginfo')(module);
 
-var GulpRunner = require('gulp-runner');
-
 var config = require('./config.js');
 var db = require('./build/how2db.js');
 var server = require('./server/server.js');
+var htmlify = require('./build/htmlify.js');
+
+const shouldBuildHtml = config.build.buildhtml;
 
 program.name('how2')
   .version(module.exports.version)
@@ -22,21 +29,10 @@ program.name('how2')
   .option('-s, --server', 'launch the documentation server')
   .parse(process.argv);
 
-if (program.build || program.watch) {
-  var gulp = new GulpRunner(path.resolve(__dirname, 'gulpfile.js'));
-  gulp.on('log', function (data) {
-    process.stdout.write(data);
-  });
-
-  gulp.on('error', function (err) {
-    process.stderr.write(err);
-  });
-
-  if (program.watch) {
-    gulp.run('watch');
-  } else if (program.build) {
-    gulp.run('build');
-  }
+if (program.build) {
+  build();
+} else if (program.watch) {
+  build();
 } else if (program.list) {
   let categories = db.GetCategories();
   categories.forEach(cat => {
@@ -93,6 +89,110 @@ if (program.build || program.watch) {
       open(howtoUrl.toString());
     } else {
       open(howtoUrl.toString());
+    }
+  });
+}
+
+function build () {
+  glob('**/*.md', {
+    cwd: slash(config.source.sourcepath)
+  }, function (err, matches) {
+    if (err) throw err;
+
+    if (shouldBuildHtml) {
+      // Cleanup output directory
+      del(config.source.outputpath + '/**/*', (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          copyAssets();
+          for (let i = 0; i < matches.length; i++) {
+            buildMarkdown(matches[i]);
+          }
+        }
+      });
+    } else {
+      for (let i = 0; i < matches.length; i++) {
+        buildMarkdown(matches[i]);
+      }
+    }
+  });
+}
+
+function copyAssets () {
+  glob('*.@(css|ico)', {
+    cwd: slash(config.source.sourcepath)
+  }, function (err, files) {
+    if (err) console.err(err);
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const srcPath = path.resolve(config.source.sourcepath, file);
+      const destPath = path.resolve(config.build.outputpath, file);
+      fs.copyFile(srcPath, destPath, (err) => {
+        if (err) console.error(err);
+      });
+    }
+  });
+
+  glob('fonts/**/*', {
+    cwd: slash(config.source.sourcepath)
+  }, function (err, files) {
+    if (err) {
+      console.err(err);
+    } else {
+      mkdirp(path.resolve(config.build.outputpath, 'fonts'), function (err) {
+        if (err) {
+          console.error(err);
+        } else {
+          for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const srcPath = path.resolve(config.source.sourcepath, file);
+            const destPath = path.resolve(config.build.outputpath, file);
+            fs.copyFile(srcPath, destPath, (err) => {
+              if (err) console.error(err);
+            });
+          }
+        }
+      });
+    }
+  });
+}
+
+function buildMarkdown (file) {
+  db.Clear();
+  const relativePath = slash(file).replace(/.md$/, '');
+  const absPath = path.resolve(config.source.sourcepath, file);
+  const pathSegments = relativePath.split('/');
+  const category = pathSegments.slice(0, -1).join('/');
+  const fileName = pathSegments[pathSegments.length - 1];
+  fs.readFile(absPath, 'utf8', function (err, data) {
+    if (err) {
+      console.error(err);
+    } else {
+      const content = frontMatter(data);
+      const metadata = content.attributes;
+      db.Insert({
+        category: category,
+        title: metadata.title,
+        tags: metadata.tags || [],
+        path: relativePath
+      });
+      if (shouldBuildHtml) {
+        var html = htmlify(content);
+        var outputDir = path.join(config.build.outputpath, category);
+        mkdirp(outputDir, function (err) {
+          if (err) {
+            console.error(err);
+          } else {
+            var outputPath = path.join(outputDir, fileName + '.html');
+            fs.writeFile(outputPath, html, function (err) {
+              if (err) {
+                console.error(err);
+              }
+            });
+          }
+        });
+      }
     }
   });
 }
