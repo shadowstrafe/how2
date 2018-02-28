d44e4a5e7e5fe7cb25d1015c667c6c8a25d0eb3d
Lee Chengkai <chengkai@tecedgesolutions.com>
2018-02-28 15:37:55 +0800

    Remove dependency on gulp

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
-var inquirer = require('inquirer');
-var open = require('opn');
+var chokidar = require('chokidar');
 var del = require('rimraf');
-var path = require('path');
-var slash = require('slash');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
 var frontMatter = require('front-matter');
+var fs = require('fs');
 var glob = require('glob');
+var inquirer = require('inquirer');
+var mkdirp = require('mkdirp');
+var open = require('opn');
+var path = require('path');
 var program = require('commander');
+var slash = require('slash');
 var url = require('url');
 require('pkginfo')(module);
 
 var config = require('./config.js');
 var db = require('./build/how2db.js');
-var server = require('./server/server.js');
 var htmlify = require('./build/htmlify.js');
+var server = require('./server/server.js');
 
 const shouldBuildHtml = config.build.buildhtml;
 
@@ -33,6 +34,27 @@ if (program.build) {
   build();
 } else if (program.watch) {
   build();
+  var watcher = chokidar.watch('**/*.md', {
+    persistent: true,
+    ignoreInitial: true,
+    cwd: config.source.sourcepath
+  });
+  watcher
+    .on('add', filePath => {
+      console.log('file added ' + filePath);
+      buildMarkdown(filePath);
+    })
+    .on('change', filePath => {
+      console.log('file changed ' + filePath);
+      change(filePath);
+    })
+    .on('unlink', filePath => {
+      console.log('file deleted ' + filePath);
+      remove(filePath);
+    })
+    .on('error', error => console.error(error));
+
+  console.log('Currently watching ' + config.source.sourcepath + ' for changes');
 } else if (program.list) {
   let categories = db.GetCategories();
   categories.forEach(cat => {
@@ -105,6 +127,7 @@ function build () {
         if (err) {
           console.error(err);
         } else {
+          db.Clear();
           copyAssets();
           for (let i = 0; i < matches.length; i++) {
             buildMarkdown(matches[i]);
@@ -112,11 +135,30 @@ function build () {
         }
       });
     } else {
+      db.Clear();
       for (let i = 0; i < matches.length; i++) {
         buildMarkdown(matches[i]);
       }
     }
   });
+}
+
+function change (filePath) {
+  var relativePath = slash(filePath).replace(/.md$/, '');
+  db.Delete(relativePath);
+  buildMarkdown(filePath);
+}
+
+function remove (filePath) {
+  var relativePath = slash(filePath).replace(/.md$/, '');
+  db.Delete(relativePath);
+
+  if (shouldBuildHtml) {
+    var destFilePath = path.join(config.build.outputpath, relativePath + '.html');
+    del(destFilePath, function (err) {
+      console.error(err);
+    });
+  }
 }
 
 function copyAssets () {
@@ -158,10 +200,9 @@ function copyAssets () {
   });
 }
 
-function buildMarkdown (file) {
-  db.Clear();
-  const relativePath = slash(file).replace(/.md$/, '');
-  const absPath = path.resolve(config.source.sourcepath, file);
+function buildMarkdown (filePath) {
+  const relativePath = slash(filePath).replace(/.md$/, '');
+  const absPath = path.resolve(config.source.sourcepath, filePath);
   const pathSegments = relativePath.split('/');
   const category = pathSegments.slice(0, -1).join('/');
   const fileName = pathSegments[pathSegments.length - 1];
