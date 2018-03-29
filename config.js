2011053cf2fe0d90da8ae19bfd2524e04efb28e4
Lee Chengkai <chengkai@tecedgesolutions.com>
2018-03-29 13:57:39 +0800

    Add new config options for assets

--- config.js
+++ config.js
@@ -5,12 +5,16 @@ config.source = config.source || {};
 config.server = config.server || {};
 config.build = config.build || {};
 
-config.source.templatepath = config.source.templatepath || path.join(config.source.sourcepath, '/how2.template');
+config.source.sourcepath = config.source.sourcepath || path.resolve(__dirname, './src/');
+config.source.assetdirpath = config.source.assetdirpath || path.resolve(__dirname, './assets/');
+config.source.templatepath = config.source.templatepath || path.resolve(config.source.assetpath, './how2.handlebars');
 
 config.server.port = config.server.port || '5500';
+config.server.launch = config.server.launch === undefined ? false : config.server.launch;
 
 // Temporarily use a hardcoded dist path for now
-config.build.outputpath = config.build.outputpath || path.resolve(__dirname, './dist');
 config.build.manifestdir = config.build.manifestdir || path.resolve(__dirname, './dist');
+config.build.outputpath = config.build.outputpath || path.resolve(__dirname, './dist');
+config.build.buildhtml = config.build.buildhtml === undefined ? false : config.build.buildhtml;
 
 module.exports = config;
