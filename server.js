63195a64721023fc136ee73178b5168020dfe006
Lee Chengkai <chengkai@tecedgesolutions.com>
2018-04-20 14:29:56 +0800

    Add theming controls backed by cookies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+var cookieParser = require('cookie-parser');
 var express = require('express');
-var path = require('path');
 var moment = require('moment');
+var path = require('path');
 
 var config = require('./config');
 var htmlify = require('./build/htmlify');
@@ -11,19 +12,54 @@ const PORT = config.server.port;
 const STATIC_ROOT = config.assetDirpath;
 const SOURCE_DIRPATH = config.sourceDirpath;
 
+const THEMES = {
+  'theme-light': {
+    cls: 'theme-light',
+    hljs: 'solarized-light.css'
+  },
+  'theme-dark': {
+    cls: 'theme-dark',
+    hljs: 'solarized-dark.css'
+  }
+};
+
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Logging
+app.use(function (req, res, next) {
+  logger.verbose('server.js: ' + req.method + ' ' + req.originalUrl);
+  next();
+});
+
+// Static Sources
 app.use(express.static(STATIC_ROOT));
+app.use('/how2', express.static(SOURCE_DIRPATH));
+
+// Setup cookie middleware
+app.use(cookieParser());
+app.use(function (req, res, next) {
+  var theme = req.cookies.how2theme;
+  logger.debug('how2theme cookie value: ' + theme);
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'How 2',
+    search: '',
+    searchAutofocus: true,
+    theme: getTheme(req)
+  });
+});
 
 app.get('/how2/*.html', function (req, res) {
-  logger.verbose('server.js: GET ' + req.originalUrl);
   try {
-    logger.debug('server.js: id=' + req.params[0]);
     var howto = db.Get(req.params[0]);
     if (!howto) {
+      logger.debug('server.js: GET how2 id=' + req.params[0] + ' Not Found');
       res.sendStatus(404);
       return;
     }
@@ -36,7 +72,8 @@ app.get('/how2/*.html', function (req, res) {
       title: howto.title,
       category: howto.category,
       search: howto.category.replace('/', ' '),
-      searchAutofocus: false
+      searchAutofocus: false,
+      theme: getTheme(req)
     };
 
     res.render('how2', templateData);
@@ -44,8 +81,6 @@ app.get('/how2/*.html', function (req, res) {
     res.send(err);
   }
 });
-
-app.use('/how2', express.static(SOURCE_DIRPATH));
 
 app.get('/search', function (req, res) {
   var q = req.query.q;
@@ -65,17 +100,29 @@ app.get('/search', function (req, res) {
     title: 'How 2',
     search: q,
     searchAutofocus: true,
-    results: results
+    results: results,
+    theme: getTheme(req)
   });
 });
 
-app.get('/', function (req, res) {
-  res.render('index', {
-    title: 'How 2',
-    search: '',
-    searchAutofocus: true
-  });
+app.post('/theme/:theme', function (req, res) {
+  var wantedTheme = 'theme-' + req.params['theme'];
+  logger.debug('Post wanted theme ' + wantedTheme);
+  if (wantedTheme && wantedTheme in THEMES) {
+    logger.debug('Setting cookie ' + wantedTheme);
+    res.cookie('theme', wantedTheme);
+  }
+  res.redirect('/');
 });
+
+function getTheme (req) {
+  var wantedTheme = req.cookies.theme;
+  if (wantedTheme && wantedTheme in THEMES) {
+    return THEMES[wantedTheme];
+  } else {
+    return THEMES['theme-light'];
+  }
+}
 
 function start () {
   app.listen(PORT, function () {
